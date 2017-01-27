c7df2ac23b6e1be1164e29f6ebbb4f160840628f
@be <abeahmed2@gmail.com>
2017-01-27 17:35:35 -0500

    Added a few comments. Removed a print statement.

--- ClubFlaskApp/server.py
+++ ClubFlaskApp/server.py
@@ -4,19 +4,21 @@ app = Flask(__name__)
 import json
 from datetime import datetime
 
+# sort the events by date, I'm sure this could be done on the client side
+# and I'd probably prefer it be done there
 events=[]
 clubs =  json.load(open('../club_events.json'))
 for club in clubs:
 	events += club['club_events']
 events.sort(key= lambda k: k['start_date_time'], reverse=True)
+
+# make the time stamps more readable
 for event in events:
 	event['start_date_time'] = datetime.strptime(event['start_date_time'], '%Y-%m-%dT%H:%M:%S%z').strftime(
 		'%A %B %d, %Y, at %I:%M%p'
 	)
 
+# single page web apps rock, tehe
 @app.route('/')
 def index():
-
-	print("Serving", len(clubs), "clubs")
-
 	return render_template('index.html', events=events)
