e99ca884fb47902af1f3afe4798fdc9829144bc2
@be <abeahmed2@gmail.com>
2017-02-02 22:27:27 -0500

    Using Scalingo to deploy this. Testing for now.

--- ClubFlaskApp/server.py
+++ ClubFlaskApp/server.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask import render_template
 app = Flask(__name__)
@@ -7,18 +8,22 @@ from datetime import datetime
 # sort the events by date, I'm sure this could be done on the client side
 # and I'd probably prefer it be done there
 events=[]
-clubs =  json.load(open('../club_events.json'))
+clubs =  json.load(open('./club_events.json'))
 for club in clubs:
-	events += club['club_events']
+    events += club['club_events']
 events.sort(key= lambda k: k['start_date_time'], reverse=True)
 
 # make the time stamps more readable
 for event in events:
-	event['start_date_time'] = datetime.strptime(event['start_date_time'], '%Y-%m-%dT%H:%M:%S%z')
-	event['date_time_str'] = event['start_date_time'].strftime('%A %B %d, %Y, at %I:%M%p')
-	event['epoch'] = int(event['start_date_time'].timestamp())
+    event['start_date_time'] = datetime.strptime(event['start_date_time'], '%Y-%m-%dT%H:%M:%S%z')
+    event['date_time_str'] = event['start_date_time'].strftime('%A %B %d, %Y, at %I:%M%p')
+    event['epoch'] = int(event['start_date_time'].timestamp())
 
 # single page web apps rock, tehe
 @app.route('/')
 def index():
-	return render_template('index.html', events=events)
+    return render_template('index.html', events=events)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
